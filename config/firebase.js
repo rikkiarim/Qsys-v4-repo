8a617bb11fd74c70e7fb527d223a66064201fdfc
Rikki Arim <rikki.arim@feedonomics.com>
2025-07-23 16:14:58 -0700

    chore: validate Firebase env vars and un-escape private key in config/firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,35 @@
+// config/firebase.js
+'use strict';
 
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // Use your key
 
+// Require and validate all needed env vars
+const {
+  FIREBASE_PROJECT_ID,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_DATABASE_URL
+} = process.env;
+
+if (!FIREBASE_PROJECT_ID ||
+    !FIREBASE_CLIENT_EMAIL ||
+    !FIREBASE_PRIVATE_KEY) {
+  throw new Error(
+    'Missing one of FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, or FIREBASE_PRIVATE_KEY env vars'
+  );
+}
+
+// Un-escape the private key newline characters
+const privateKey = FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
+
+// Initialize the Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId:   FIREBASE_PROJECT_ID,
+    clientEmail: FIREBASE_CLIENT_EMAIL,
+    privateKey:  privateKey
+  }),
+  databaseURL: FIREBASE_DATABASE_URL  // optional, if you use RTDB
 });
 
 module.exports = admin;
