635f12328c204f60ea1368f7bbff554085e7c922
Rikki Arim <rikki.arim@feedonomics.com>
2025-07-29 10:17:26 -0700

    fix: remove all file logging, use console-only logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,27 +1,6 @@
 // utils/logger.js
-const { createLogger, format, transports } = require('winston');
-const path = require('path');
-
-const logger = createLogger({
-  level: 'debug',
-  format: format.combine(
-    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    format.printf(({ timestamp, level, message }) =>
-      `${timestamp} [${level.toUpperCase()}] ${message}`
-    )
-  ),
-  transports: [
-    // write all logs (debug and up) to debug.log at project root
-    new transports.File({ filename: path.join(__dirname, '../debug.log'), level: 'debug' }),
-    // also show errors on console
-    new transports.Console({ level: 'error', format: format.simple() })
-  ],
-});
-
-// utils/logger.js
 
 module.exports = {
   debug: (...args) => console.log('[DEBUG]', ...args),
   error: (...args) => console.error('[ERROR]', ...args),
 };
-
